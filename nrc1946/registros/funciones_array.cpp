18d39a3fa57fbd565abee7c6f5d108fbcfa2ca19
Lennon <lennon.albuja@gmail.com>
2025-02-21 10:06:59 -0500

    Q

---
+++ nrc1946/registros/funciones_array.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+struct persona{
+    int id;
+    string nombre;
+    int edad;
+    bool casado=false;
+};
+
+void ingresarDatosRegistro(persona per[], int n){
+    int i;
+    char resp;
+    for(i = 0; i < n; i++){
+        per[i].id = i+1;
+        cout << "Ingrese el nombre de la persona: ";
+        cin >> per[i].nombre;
+        cout << "Ingrese la edad de la persona: ";
+        cin >> per[i].edad;
+        cout << "Es casado?: <<S/N>>: ";
+        cin.get(resp);
+        cin.ignore();
+        if(resp=='s' || resp=='S')
+            per[i].casado = true;
+    }
+}
+
+void mostrarDatosRegistro(persona per[], int n){
+    int i;
+    cout << "ID\tNOMBRE\tEDAD\tCASADO"<<endl;
+    for(i=0;i<n;i++){
+        cout << per[i].id << "\t";
+        cout << per[i].nombre << "\t";
+        cout << per[i].edad << "\t";
+        cout << per[i].casado<<endl;
+    }
+
+}
+
+main(){
+    int np;
+    cout << "Ingrese la cantidad de personas: ";
+    cin >> np;
+    persona personas[np];
+    ingresarDatosRegistro(personas, np);
+    mostrarDatosRegistro(personas, np);
+}
